b8fcc6fbb6e01eebbd521b9fa4b8e2984c8c1f24
amurrill <59675275+amurrill@users.noreply.github.com>
2020-02-17 22:43:59 -0600

    Add files via upload

---
+++ tutorial-part-two2/gatsby-browser.js
@@ -0,0 +1,3 @@
+require('./src/styles/global.css')
+// or:
+// import "./src/styles/global.css"
---
+++ tutorial-part-two2/src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Link } from "gatsby"
+export default () => <div>
+    <Link to = "/about-css-modules/">About-css-modules</Link>
+    <h1>Hello world!</h1>
+</div>
