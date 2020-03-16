96929c72bf97c975d5d55c26bc601ec3d2907260
amurrill <59675275+amurrill@users.noreply.github.com>
2020-03-15 22:46:48 -0500

    Add files via upload

--- PortfolioResume/sw.js
+++ PortfolioResume/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(e){
 				'/PortfolioResume/index.js',
 				'/PortfolioResume/css/w3.css',
 				'/PortfolioResume/images/code.jpg',
-				'/PortfolioResume/images/computer-monitor.jpg'
+				'/PortfolioResume/images/computer-monitor.jpg',
 				'/PortfolioResume/images/icon.png'
 			]);	
 		})
